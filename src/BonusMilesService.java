e0420134f7e352eae1fe2b4b244e91b27391aaaf
Valeriia <lera-bezborodova@mail.ru>
2020-04-05 21:04:26 +0300

    Fixed comments

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
-    public int calculate (int price) {
-        int bonusmiles = 20;//цена одной мили
-        int miles = price / bonusmiles;//кол-во бонусных миль за билет
+    public int calculate(int price) {
+        int bonusMiles = 20;//цена одной мили
+        int miles = price / bonusMiles;//кол-во бонусных миль за билет
         return miles;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
         int price = 10_000;
         int miles = service.calculate(price);
         System.out.println(miles);
-            }
+    }
 }
 
